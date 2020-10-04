70de5eeaea7daca6f2e7ff647d9d39b5750de3ca
rhlmalik17 <rhlmalik17@gmail.com>
2020-10-04 11:03:13 +0530

    initial commit

---
+++ src/portfolio-container/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { TOTAL_SCREENS } from '../../utilities/commonUtils';
+import './Header.css';
+export const Header = () => {
+
+    const [selectedScreen, setSelectedScreen] = useState(0);
+    const getHeaderOptions = () => {
+        return (
+          TOTAL_SCREENS.map((Screen,i) => (
+            <div className={getHeaderOptionsClasses(i)}
+                onClick={() => switchScreen(i)}
+            >
+                 <span>{Screen.screen_name}</span>
+            </div>
+          ))
+        )
+    }
+
+    const getHeaderOptionsClasses = (index) => {
+        let classes = "header-option ";
+        if(index < TOTAL_SCREENS.length-1)
+        classes+= "header-option-seperator ";
+
+        if(selectedScreen === index)
+        classes+= "selected-header-option ";
+        
+        return classes;
+    }
+
+    const switchScreen = (index) => {
+        setSelectedScreen(index);
+    }
+
+    return (
+        <div className="header-container">
+            <div className="header-parent">
+                <div className="header-logo">
+                    <span>RM.</span>
+                </div>
+                <div className="header-options">
+               { getHeaderOptions() }
+            </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/utilities/commonUtils.js
@@ -0,0 +1,9 @@
+
+/* TOTAL NUMBER OF SCREENS */
+export const TOTAL_SCREENS = [
+    { screen_name: "Home" },
+    { screen_name: "About Me" },
+    { screen_name: "Resume" },
+    { screen_name: "Portfolio" },
+    { screen_name: "Contract" }
+];
